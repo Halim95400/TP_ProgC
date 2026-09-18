abbd20f3bc13f4ab9ea93be52e5eec5eb0db7df5
Halim95400 <halim.ciftci9@gmail.com>
2026-09-18 09:19:22 +0000

    TP2 - Exercice 2.2

---
+++ TP1/src/bits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int d = 0x90001000;
+
+    unsigned int bit4 = (d >> 28) & 1;
+    unsigned int bit20 = (d >> 12) & 1;
+
+    if (bit4 == 1 && bit20 == 1) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    return 0;
+}
+
